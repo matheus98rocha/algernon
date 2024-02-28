e52467309c065e7174b4e30921bc3fb7a4a34b36
matheus98rocha <matheus98rocha@gmail.com>
2024-02-27 21:19:06 -0300

    feat: Reiniciando o project com nativewind

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-yellow-500 ">
+      <Text className="text-xl text-white">index</Text>
+    </View>
+  );
+}
+
+export default Home;
