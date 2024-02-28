d18c72fece6c7e51806afd5eba24d00b7b716c55
matheus98rocha <matheus98rocha@gmail.com>
2024-02-27 22:10:08 -0300

    feat: adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [
+    'universe/native',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react-native', 'tailwindcss'],
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'padded-blocks': 'off',
+    'arrow-body-style': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+  },
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
 
 function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-yellow-500 ">
-      <Text className="text-xl text-white">index</Text>
+    <View className="flex-1 items-center justify-center bg-yellow-200">
+      <Text className="text-xl text-black">index</Text>
     </View>
   );
 }
